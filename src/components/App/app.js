3bd7c707cbe79b50541062ff82f41ae67fa4257d
MinnaAH <m.hannula@elisanet.fi>
2021-01-05 12:08:47 +0200

    Bug fixes and jest test configuration

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -8,20 +8,36 @@ import Details from '../Details/details';
 import './app.css';
 
 const App = () => {
-  const dispatch = useDispatch();
+    let thumbnails;
+    const dispatch = useDispatch();
     const thumbnailsPerPage = 10;
     const [currentPage, setCurrentPage] = useState(1);
     const photos = useSelector(state => state.photos.photos);
-  
-    const thumbnails = photos.map((photo) => {
-      return <Link key={photo.id} to={`/photo/${photo.id}`}><Thumbnail thumbnail={photo} /></Link>;
-    });
-  
+    const loading = useSelector(state => state.photos.loading);
+    const error = useSelector(state => state.photos.error);
+
+    if (photos.length > 0 ) {
+      thumbnails = photos.map((photo) => {
+        return <Link key={photo.id} to={`/photo/${photo.id}`}><Thumbnail thumbnail={photo} /></Link>;
+      });
+    }
+
     useEffect(() => {
       dispatch(getPhotos((currentPage-1)*thumbnailsPerPage));
-    })
+    },[currentPage]);
+
     return(
       <div>
+        <div style={{ display: !error && !loading && photos.length > 0? "none" : "block" }} className="frame">
+          <div style={{ display: loading ? "block" : "none" }} className="loader"></div>
+          <p style={{ display: error && !loading ? "block" : "none" }} className="error">
+            Error occured on page! <br/>
+            {error}
+          </p>
+          <p style={{ display: !error && !loading && photos.length === 0 ? "block" : "none" }} className="noPhotos">
+            No photos found
+          </p>
+        </div>
       <Link to="/"></Link>
       <Link to="/photo/:photoId"></Link>
       <Switch>
